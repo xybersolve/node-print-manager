efe98666197b219a58285693151944b9d1909d93
xybersolve <1531070+xybersolve@users.noreply.github.com>
2018-11-16 13:05:52 -0800

    update remote sync

---
+++ deploy.config.sh
@@ -0,0 +1,5 @@
+declare -r GIT_REPO="git@github.com:xybersolve/node-print-manager.git"
+declare -r APP_DIRECTORY='/home/ubuntu/node/print-manager'
+declare -r SERVER_IP='52.13.196.106'
+declare -r USER='ubuntu'
+declare HOST='control'
