1c404b1e40f6cb521c92f511de3e4eeae01b5605
xybersolve <1531070+xybersolve@users.noreply.github.com>
2018-11-16 13:11:40 -0800

    add image REST endpoints

--- src/providers/images-provider.js
+++ src/providers/images-provider.js
@@ -1,9 +1,22 @@
 let db = null
 let images = null
 
+// using single owner from now
+const owner = 'Greg Milligan'
+
 module.exports = (DB) => {
   db = DB.db
   images = db.collection('images')
+
+  // suport routines
+  // get next id in the image collection for this owner
+  const getNextId = (cb) => {
+    images.find({ owner: owner }, { _id: 0, id: 1 }).sort({ id: -1 }).limit(1).toArray((err, result) => {
+      if (err) return cb(err)
+      let nextId = +result[0].id + 1
+      cb(null, nextId)
+    })
+  }
 
   return {
     getAll: () => {
@@ -16,12 +29,51 @@ module.exports = (DB) => {
       })
     },
     getImage: (id) => {
-      console.log(`image id: ${id}`)
+      console.log(`provider:id: ${id}`)
       return new Promise((resolve, reject) => {
-        images.find({ id }).toArray().then((image) => {
+        images.findOne({ id: +id, owner: owner }).then(image => {
           resolve(image)
-        }).catch((err) => {
+        }).catch(err => {
           reject(err)
+        })
+      })
+    },
+    createImage: ({ data, owner }) => {
+      console.dir(data)
+      data.owner = owner
+      return new Promise((resolve, reject) => {
+        getNextId((err, nextId) => {
+          if (err) return reject(err)
+          data.id = nextId
+          // resolve(data)
+          images.insertOne(data).then((result) => {
+            // {"n":1,"ok":1}
+            resolve(result)
+          }).catch((err) => {
+            reject(err)
+          })
+        })
+      })
+    },
+    deleteImage: ({ id, owner }) => {
+      // console.log(`provider:id: ${id}, owner: ${owner}`)
+      return new Promise((resolve, reject) => {
+        images.deleteOne(
+          { id: id, owner: owner },
+          { w: 0, j: true }).then(result => {
+          // {"n":0,"ok":1}
+          resolve(result.result)
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    },
+    // just a test stub
+    getNextId: () => {
+      return new Promise((resolve, reject) => {
+        getNextId((err, nextId) => {
+          if (err) return reject(err)
+          resolve({ id: nextId })
         })
       })
     }
