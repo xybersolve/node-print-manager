50df0660390393fe6ff74bae413329a22828ff4e
xybersolve <1531070+xybersolve@users.noreply.github.com>
2018-11-16 13:11:57 -0800

    add image REST endpoints

--- src/routes/images.js
+++ src/routes/images.js
@@ -4,6 +4,7 @@
 const express = require('express')
 const router = express.Router()
 let images = null
+let owner = 'Greg Milligan'
 
 // return all images
 router.get('/', (req, res, next) => {
@@ -12,10 +13,43 @@ router.get('/', (req, res, next) => {
   }).catch(next)
 })
 
+// test - get next id
+router.get('/next-id', (req, res, next) => {
+  images.getNextId().then(result => {
+    console.dir(result)
+    res.send(result)
+  }).catch(next)
+})
+
 // return image by id
 router.get('/:id', (req, res, next) => {
-  images.getImage(req.params.id).then((image) => {
+  console.log(`route:id: ${req.params.id}`)
+  images.getImage(req.params.id).then(image => {
+    console.dir(image)
     res.send(image)
+  }).catch(next)
+})
+
+// create new image - return
+router.post('/', (req, res, next) => {
+  var opts = {
+    owner,
+    data: req.body
+  }
+  images.createImage(opts).then(result => {
+    res.json(result)
+  }).catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  var opts = {
+    owner,
+    id: req.params.id
+  }
+  images.deleteImage(opts).then(result => {
+    // { n: 0, ok: 1 }
+    console.log(`route - ok:${result.ok}, n:${result.n}`)
+    res.json(result)
   }).catch(next)
 })
 
