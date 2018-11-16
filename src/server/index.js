d4feafe7d1f6dfc32451a20c827bfe9cfabafb34
xybersolve <1531070+xybersolve@users.noreply.github.com>
2018-11-16 13:08:57 -0800

    start server split out refactor

---
+++ src/server/index.js
@@ -0,0 +1,9 @@
+// const express = require('express')
+// const morgan = require('morgan')
+// const helmet = require('helmet')
+//
+// const start = (options) => {
+//   return new Promise((resolve, reject) => {
+//
+//   })
+// }
